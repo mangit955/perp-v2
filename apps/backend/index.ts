4fd446f43f150af54a21b349ae7fcd1c6d5e7780
Manas Raghuwanshi <manasr955@gmail.com>
2026-06-10 14:34:58 +0530

    impleneted signin tests

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,7 +1,22 @@
+import { prisma } from "db/client";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.post("api/v1/sihnup", (req, res) => {});
+app.post("/api/v1/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(411).json("enter username & password");
+  }
+
+  const response = await prisma.users.create({
+    data: { username, password },
+  });
+  res.json({
+    id: response.id,
+  });
+});
 
 app.listen(3000);
--- apps/tests/index.test.ts
+++ apps/tests/index.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, it } from "bun:test";
 import axios, { AxiosError } from "axios";
 import { BACKEND } from "./config";
-import { password } from "bun";
 
 describe("auth endpoints", () => {
   const username = `manas ${Math.random()}`;
@@ -11,12 +10,12 @@ describe("auth endpoints", () => {
         password: "123234",
       });
 
-      expect(1).toBe(2);
+      expect().fail();
     } catch (e) {
       if (e instanceof AxiosError) {
         expect(e.response?.status).toBe(411);
       } else {
-        expect(1).toBe(2);
+        expect().fail();
       }
     }
   });
@@ -27,5 +26,50 @@ describe("auth endpoints", () => {
       password: "123234",
     });
     expect(response.data.id).not.toBe(undefined);
+  });
+
+  //bad input
+  it("Signin doesn't work if u don't provide username", async () => {
+    try {
+      const response = await axios.post(`${BACKEND}/api/v1/signin`, {
+        password: "123234",
+      });
+
+      expect().fail();
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        expect(e.response?.status).toBe(411);
+      } else {
+        expect().fail();
+      }
+    }
+  });
+
+  //incorrect credentials
+  it("Signin doesn't work if wrong credentials are sent", async () => {
+    try {
+      const response = await axios.post(`${BACKEND}/api/v1/signin`, {
+        username,
+        password: "123234",
+      });
+
+      expect().fail();
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        expect(e.response?.status).toBe(403);
+      } else {
+        expect().fail();
+      }
+    }
+  });
+
+  //correct once
+  it("Signin does work if right credentials are sent", async () => {
+    const response = await axios.post(`${BACKEND}/api/v1/signup`, {
+      username,
+      password: "123234",
+    });
+    expect(response.status).toBe(200);
+    expect(response.data.token).not.toBe(undefined);
   });
 });
