9e34de8f06b27a7df08a95ef126e5bafb1bf6e3b
Manas Raghuwanshi <manasr955@gmail.com>
2026-06-11 09:30:44 +0530

    added onramp & order routes with tests

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,5 +1,11 @@
 import { prisma } from "db/client";
 import express from "express";
+import jwt from "jsonwebtoken";
+import { authMiddleware } from "./middleware";
+import { createClient } from "redis";
+
+const client = createClient();
+client.connect();
 
 const app = express();
 app.use(express.json());
@@ -18,5 +24,46 @@ app.post("/api/v1/signup", async (req, res) => {
     id: response.id,
   });
 });
+
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(411).json("enter username & password");
+  }
+
+  const response = await prisma.users.findFirst({
+    where: { username, password },
+  });
+
+  if (!response) {
+    res.status(403).json({ message: "Incorrect creds" });
+  }
+  res.json({
+    token: jwt.sign({ userId: response?.id }, process.env.JWT_SECRET!),
+  });
+});
+
+app.post("/admin/market", async (req, res) => {
+  const { symbol, imageUrl } = req.body;
+  const token = req.headers.token;
+
+  if (token != process.env.ADMIN_SECRET) {
+    res.status(403).json({});
+    return;
+  }
+
+  const response = await prisma.market.create({
+    data: {
+      slug: symbol,
+      imageUrl,
+    },
+  });
+  res.json({ id: response.id });
+});
+
+app.post("/api/v1/onramp", authMiddleware, (req, res) => {});
+
+app.post("/api/v1/order", authMiddleware, (req, res) => {});
 
 app.listen(3000);
--- apps/tests/index.test.ts
+++ apps/tests/index.test.ts
@@ -1,6 +1,8 @@
-import { describe, expect, it } from "bun:test";
+import { Users } from "./../../packages/db/generated/prisma/browser";
+import { beforeAll, describe, expect, it } from "bun:test";
 import axios, { AxiosError } from "axios";
 import { BACKEND } from "./config";
+import { password } from "bun";
 
 describe("auth endpoints", () => {
   const username = `manas ${Math.random()}`;
@@ -50,7 +52,7 @@ describe("auth endpoints", () => {
     try {
       const response = await axios.post(`${BACKEND}/api/v1/signin`, {
         username,
-        password: "123234",
+        password: "wrong-password",
       });
 
       expect().fail();
@@ -65,11 +67,146 @@ describe("auth endpoints", () => {
 
   //correct once
   it("Signin does work if right credentials are sent", async () => {
-    const response = await axios.post(`${BACKEND}/api/v1/signup`, {
+    const response = await axios.post(`${BACKEND}/api/v1/signin`, {
       username,
       password: "123234",
     });
     expect(response.status).toBe(200);
     expect(response.data.token).not.toBe(undefined);
+  });
+});
+
+describe("Order endpoints", () => {
+  const USER_1 = `manas-${Math.random()}`;
+  const USER_2 = `manas-${Math.random()}`;
+  const PASSWORD = "random123";
+  let MARKET_ID: string = "";
+  let user1Token: string = "";
+  let user2Token: string = "";
+
+  beforeAll(async () => {
+    await axios.post(
+      `${BACKEND}/admin/market`,
+      {
+        symbol: "SOL",
+        imageUrl: "sol.png",
+      },
+      {
+        headers: {
+          token: "Manas123",
+        },
+      },
+    );
+    await axios.post(`${BACKEND}/api/v1/signup`, {
+      username: USER_1,
+      password: PASSWORD,
+    });
+    await axios.post(`${BACKEND}/api/v1/signup`, {
+      username: USER_2,
+      password: PASSWORD,
+    });
+
+    const response1 = await axios.post(`${BACKEND}/api/v1/signin`, {
+      username: USER_1,
+      password: PASSWORD,
+    });
+
+    const response2 = await axios.post(`${BACKEND}/api/v1/signin`, {
+      username: USER_2,
+      password: PASSWORD,
+    });
+
+    user1Token = response1.data.token;
+    user2Token = response2.data.token;
+
+    await axios.post(
+      `${BACKEND}/api/v1/onramp`,
+      {
+        amount: 100000,
+      },
+      {
+        headers: {
+          token: user1Token,
+        },
+      },
+    );
+
+    await axios.post(
+      `${BACKEND}/api/v1/onramp`,
+      {
+        amount: 100000,
+      },
+      {
+        headers: {
+          token: user2Token,
+        },
+      },
+    );
+  });
+
+  it("First order should sit on the book with 0 qty", async () => {
+    const response = await axios.post(
+      `${BACKEND}/api/v1/order`,
+      {
+        price: 100,
+        qty: 10,
+        side: "long",
+        marketId: MARKET_ID,
+        type: "limit",
+        equity: 1000,
+      },
+      {
+        headers: {
+          token: user1Token,
+        },
+      },
+    );
+    expect(response.status).toBe(200);
+    expect(response.data.filledQty).toBe(0);
+    expect(response.data.orderId).toBeDefined();
+  });
+
+  it("Second order shpold sit on the book if not matched", async () => {
+    const response = await axios.post(
+      `${BACKEND}/api/v1/order`,
+      {
+        price: 102,
+        qty: 10,
+        side: "short",
+        marketId: MARKET_ID,
+        type: "limit",
+        equity: 1000,
+      },
+      {
+        headers: {
+          token: user1Token,
+        },
+      },
+    );
+    expect(response.status).toBe(200);
+    expect(response.data.filledQty).toBe(0);
+    expect(response.data.orderId).toBeDefined();
+  });
+
+  it("Third order should match", async () => {
+    const response = await axios.post(
+      `${BACKEND}/api/v1/order`,
+      {
+        price: 100,
+        qty: 20,
+        side: "short",
+        marketId: MARKET_ID,
+        type: "limit",
+        equity: 1000,
+      },
+      {
+        headers: {
+          token: user1Token,
+        },
+      },
+    );
+    expect(response.status).toBe(200);
+    expect(response.data.filledQty).toBe(10);
+    expect(response.data.orderId).toBeDefined();
   });
 });
